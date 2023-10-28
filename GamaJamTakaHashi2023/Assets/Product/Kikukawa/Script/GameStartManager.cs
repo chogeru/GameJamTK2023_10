254f9290165e2d2d648108b5217b6f61f6500dec
MKTkakumei <nizigenjyasinmkt@gmail.com>
2023-10-28 15:02:33 +0900

    ・菊川の見本Scriptを編集

--- GamaJamTakaHashi2023/Assets/Product/Kikukawa/Script/GameStartManager.cs
+++ GamaJamTakaHashi2023/Assets/Product/Kikukawa/Script/GameStartManager.cs
@@ -8,11 +8,19 @@ using UnityEngine;
 
 namespace Kikukawa {
     public class GameStartManager : SingletonMonoBehaviour<GameStartManager> {
+        public bool Test = false;
+
         void Start() {
             Manager.BGMManager.Instance.FadeBGMChange("OniBaraiBGM01");
-            Manager.SEManager.Instance.SEPlay("10Combo");
             Manager.FadeManager.Instance.SetFadeColor(new Color(0.0f, 0.0f, 0.0f, 1.0f));
             Manager.FadeManager.Instance.SetFadeFlag(false);
+        }
+       void Update() {
+            if (Test) {
+                Manager.ParticleManager.Instance.ParticlePlay("BigHeart", new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 0),Quaternion.identity,2.0f);
+                Manager.SEManager.Instance.SEPlay("10Combo");
+                Test = false;
+            }
         }
     }
 }
